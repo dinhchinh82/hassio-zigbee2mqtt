e82631c4ec536bfde44a88692f089ecaa55ad25f
Chinh Nguyen <dinhchinh@gmail.com>
2021-03-16 23:20:44 +0700

    fix rèm

--- common/rootfs/etc/cont-init.d/fix_devices.sh
+++ common/rootfs/etc/cont-init.d/fix_devices.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/with-contenv bashio
 DATA_PATH=$(bashio::config 'data_path')
 cp -f "$DATA_PATH"/devices.js /app/node_modules/zigbee-herdsman-converters/devices.js
+cp -f "$DATA_PATH"/tuya.js /app/node_modules/zigbee-herdsman-converters/lib/tuya.js
+cp -f "$DATA_PATH"/fromZigbee.js /app/node_modules/zigbee-herdsman-converters/converters/fromZigbee.js
+cp -f "$DATA_PATH"/toZigbee.js /app/node_modules/zigbee-herdsman-converters/converters/toZigbee.js
